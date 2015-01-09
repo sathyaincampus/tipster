541a924c27f97d8ab544cafef4846f795cc4baa3
Sathya Srinivasan <s.sathyanarayanan@gmail.com>
2015-01-09 00:17:28 -0800

    Change: Working copy of Tip Calculator Project. Initial Commit Reviewed By: Sathya

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # tipster
+
 Tip Calculator Application Using Swift
+
+* This application shows how we calculate tip as and when we type the bill amount.
+* There are 3 configured tip percentages 10, 15 and 25.
+* Based on the amount entered and tip percentage selected from the Segmented control, the tip amount and total amount are calculated.
+
+We have used the following UI components :
+
+* TextField
+* UILabel
+* Tap Gesture Recognizer
+* View
+* Segmented Control
--- tipster/ViewController.swift
+++ tipster/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Setting the initial value of Tip and Total Labels to $0.00
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // This method is to update the total whenever the tip or bill amount is changed. We are binding this method to text field's edit change event as well as segmented control's selection changed event
+    @IBAction func onValueChanged(sender: AnyObject) {
+        // Since swift doesn't have toDouble we are casting the textField value 
+        // as NSString and using Objective-C syntax to convert to double
+        
+        var billAmount = (billField.text as NSString).doubleValue
+        var tipPercentages = [0.10,0.15,0.25]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        var tipAmount = billAmount * tipPercentage
+        var totalAmount = billAmount + tipAmount
+        
+        // Formatting the totalAmount and tipAmount and setting it back to the labels
+        tipLabel.text = String(format: "$%.2f", tipAmount)
+        totalLabel.text = String(format: "$%.2f", totalAmount)
+        
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
